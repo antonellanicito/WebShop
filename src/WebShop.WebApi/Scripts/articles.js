e93f5972843a1917944a9a292f514c2bde748cd5
antonellanicito <antonella.nicito@gmail.com>
2014-10-19 20:41:13 +0200

    removed not used code

--- src/WebShop.WebApi/Scripts/articles.js
+++ src/WebShop.WebApi/Scripts/articles.js
@@ -88,13 +88,3 @@
          
       }
 
-      function find() {
-          var id = $('#prodId').val();
-          $.getJSON(uri + '/' + id)
-          .done(function (data) {
-              $('#product').text(formatItem(data));
-          })
-          .fail(function (jqXHR, textStatus, err) {
-              $('#product').text('Error: ' + err);
-          });
-      }
